d80d4d37ce9a11b27f222243836cd66657342a06
andreyod <andreyo@il.ibm.com>
2016-05-31 08:54:25 +0300

    cleaned up comments in executor

--- pkg/multiTenancyPlugins/executor.go
+++ pkg/multiTenancyPlugins/executor.go
@@ -12,7 +12,6 @@ import (
 	"github.com/docker/swarm/pkg/multiTenancyPlugins/authorization/utils"
 	"github.com/docker/swarm/pkg/multiTenancyPlugins/naming"
 	"github.com/docker/swarm/pkg/multiTenancyPlugins/pluginAPI"
-	"github.com/docker/swarm/pkg/multiTenancyPlugins/utils"
 )
 
 //Executor - Entry point to multi-tenancy plugins
@@ -23,7 +22,10 @@ var startHandler pluginAPI.Handler
 //Handle - Hook point from primary to plugins
 func (*Executor) Handle(cluster cluster.Cluster, swarmHandler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Debug(r)
+		if os.Getenv("SWARM_MULTI_TENANT") == "false" {
+			swarmHandler.ServeHTTP(w, r)
+			return
+		}
 		err := startHandler(utils.ParseCommand(r), cluster, w, r, swarmHandler)
 		if err != nil {
 			log.Error(err)
@@ -42,11 +44,8 @@ func (*Executor) Init() {
 		log.Debug("Keystone not supported")
 	} else {
 		authorizationPlugin := new(authorization.DefaultAuthZImpl)
-		//flavorsPlugin := flavors.NewPlugin(authorizationPlugin.Handle)
-		//nameScoping := namescoping.NewNameScoping(flavorsPlugin.Handle)
 		nameScoping := namescoping.NewNameScoping(authorizationPlugin.Handle)
 		flavorsPlugin := flavors.NewPlugin(nameScoping.Handle)
-		//authenticationPlugin := authentication.NewAuthentication(nameScoping.Handle)
 		authenticationPlugin := authentication.NewAuthentication(flavorsPlugin.Handle)
 		startHandler = authenticationPlugin.Handle
 	}
