bd8c2ae1273737176dbca52af30a0c4b3d324bd1
Kenneth Nagin <nagin@il.ibm.com>
2016-06-29 14:21:23 +0300

    check referencing gofmt fix

--- pkg/multiTenancyPlugins/naming/nameScoping.go
+++ pkg/multiTenancyPlugins/naming/nameScoping.go
@@ -4,9 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"io/ioutil"
-	"net/http"
-	"strings"	
 	"fmt"
 	log "github.com/Sirupsen/logrus"
 	apitypes "github.com/docker/engine-api/types"
@@ -16,10 +13,12 @@ import (
 	"github.com/docker/swarm/pkg/multiTenancyPlugins/utils"
 	"github.com/gorilla/mux"
 	"github.com/samalba/dockerclient"
-
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
-const  NOTAUTHORIZED_ERROR = "Not authorized error. Tenant does not own container %s."
+const NOTAUTHORIZED_ERROR = "Not authorized error. Tenant does not own container %s."
 
 //AuthenticationImpl - implementation of plugin API
 type DefaultNameScopingImpl struct {
@@ -81,7 +80,7 @@ func (nameScoping *DefaultNameScopingImpl) Handle(command utils.CommandEnum, clu
 			newQuery = strings.Replace(r.RequestURI, r.URL.Query().Get("name"), r.URL.Query().Get("name")+r.Header.Get(headers.AuthZTenantIdHeaderName), 1)
 			containerConfig.Labels[headers.OriginalNameLabel] = r.URL.Query().Get("name")
 		}
-		if err := CheckContainerReferences(cluster,r.Header.Get(headers.AuthZTenantIdHeaderName),&containerConfig); err != nil {
+		if err := CheckContainerReferences(cluster, r.Header.Get(headers.AuthZTenantIdHeaderName), &containerConfig); err != nil {
 			log.Error(err)
 			return err
 		}
@@ -132,150 +131,149 @@ func (nameScoping *DefaultNameScopingImpl) Handle(command utils.CommandEnum, clu
 	return nil
 }
 
-func CheckContainerReferences(cluster cluster.Cluster, tenantId string, containerConfig *dockerclient.ContainerConfig) (error) {
-	log.Debugf("CheckContainerReferences containerConfig: %+v",containerConfig)
+func CheckContainerReferences(cluster cluster.Cluster, tenantId string, containerConfig *dockerclient.ContainerConfig) error {
+	log.Debugf("CheckContainerReferences containerConfig: %+v", containerConfig)
 	// create arrays of container references to pass to getIDsFromContainerReferences
-	// create volumesFrom array	
+	// create volumesFrom array
 	volumesFrom := containerConfig.HostConfig.VolumesFrom
-	
+
 	// create affinity array
-	affinityRef := make([]string,0)
+	affinityRef := make([]string, 0)
 	env := containerConfig.Env
 	var affinityIndex int
 	affinityLabelFirst := false
 	for i, e := range env {
-		if strings.HasPrefix(e,"affinity:") {
-		  if strings.HasPrefix(e,"affinity:image==") {
-			break  // ignore affinity for images 
-		  } else if strings.HasPrefix(e,"affinity:container==") {
-			containerRefIndex := strings.Index(e,"affinity:container==")+len("affinity:container==")
-			affinityRef = append(affinityRef,e[containerRefIndex:len(e)])
-			affinityIndex = i
-			break
-		  } else { // affinity:<label>:<value>
-			labelRefIndex := strings.Index(e,"affinity:")+len("affinity:")
-			affinityRef = append(affinityRef,e[labelRefIndex:len(e)])
-			affinityIndex = i
-			affinityLabelFirst = true
-			break
-		  }
+		if strings.HasPrefix(e, "affinity:") {
+			if strings.HasPrefix(e, "affinity:image==") {
+				break // ignore affinity for images
+			} else if strings.HasPrefix(e, "affinity:container==") {
+				containerRefIndex := strings.Index(e, "affinity:container==") + len("affinity:container==")
+				affinityRef = append(affinityRef, e[containerRefIndex:])
+				affinityIndex = i
+				break
+			} else { // affinity:<label>:<value>
+				labelRefIndex := strings.Index(e, "affinity:") + len("affinity:")
+				affinityRef = append(affinityRef, e[labelRefIndex:])
+				affinityIndex = i
+				affinityLabelFirst = true
+				break
+			}
 		}
 	}
-	
-	// create links array 
-	links := make([]string,0) 
+
+	// create links array
+	links := make([]string, 0)
 	for _, link := range containerConfig.HostConfig.Links {
-		linkSplit := strings.SplitN(link,":",2)
-		links = append(links,strings.TrimSpace(linkSplit[0]))
+		linkSplit := strings.SplitN(link, ":", 2)
+		links = append(links, strings.TrimSpace(linkSplit[0]))
 	}
-	
+
 	var m map[string]string
 	var err error
-	containerRefs := make([]string,0)
-	containerRefs = append(containerRefs,affinityRef...)  
-	containerRefs = append(containerRefs,volumesFrom...)  
-	containerRefs = append(containerRefs,links...)		
-	if m,err = getIDsFromContainerReferences(cluster, tenantId, containerRefs,affinityLabelFirst); err != nil {
+	containerRefs := make([]string, 0)
+	containerRefs = append(containerRefs, affinityRef...)
+	containerRefs = append(containerRefs, volumesFrom...)
+	containerRefs = append(containerRefs, links...)
+	if m, err = getIDsFromContainerReferences(cluster, tenantId, containerRefs, affinityLabelFirst); err != nil {
 		return err
 	}
 	// update containerConfig
 	// update VolumesFrom
-	for i,k := range containerConfig.HostConfig.VolumesFrom {
-		containerConfig.HostConfig.VolumesFrom[i] = m[k]			
+	for i, k := range containerConfig.HostConfig.VolumesFrom {
+		containerConfig.HostConfig.VolumesFrom[i] = m[k]
 	}
 	// update affinity
 	if len(affinityRef) > 0 {
 		containerConfig.Env[affinityIndex] = "affinity:container==" + m[affinityRef[0]]
 	}
 	// update links
-	links = make([]string,0)
-	linkSet := make(map[string]bool) 
+	links = make([]string, 0)
+	linkSet := make(map[string]bool)
 	for _, link := range containerConfig.HostConfig.Links {
-		linkSplit := strings.SplitN(link,":",2)
+		linkSplit := strings.SplitN(link, ":", 2)
 		containerIdName := strings.TrimSpace(linkSplit[0])
 		containerId := m[containerIdName]
 		if containerId != containerIdName {
-			linkSet[containerId + ":" + containerIdName] = true
+			linkSet[containerId+":"+containerIdName] = true
 		}
 		if len(linkSplit) > 1 {
-			linkSet[containerId + ":" + strings.TrimSpace(linkSplit[1])] = true
+			linkSet[containerId+":"+strings.TrimSpace(linkSplit[1])] = true
 		}
-		for k,_ := range linkSet {
-			links = append(links,k)
-		}		
+		for k := range linkSet {
+			links = append(links, k)
+		}
 	}
 	containerConfig.HostConfig.Links = links
 	return nil
 
 }
-func getIDsFromContainerReferences(cluster cluster.Cluster, tenantId string, containerReferences[] string, affinityLabelFirst bool ) (map[string]string, error) {
+func getIDsFromContainerReferences(cluster cluster.Cluster, tenantId string, containerReferences []string, affinityLabelFirst bool) (map[string]string, error) {
 	// containerReferences is a array of container references of the form long id, short id, or name
 	// if affinityLabelFirst is true the first element of containerReferences an affinity label reference of the form label:value
 	m := make(map[string]string)
 	for _, containerReference := range containerReferences {
 		m[containerReference] = ""
-	} 
+	}
 	for _, container := range cluster.Containers() {
 		if container.Labels[headers.TenancyLabel] == tenantId {
-		  for i, containerReference := range containerReferences {
-			var err error
-			var containerId string
-			if affinityLabelFirst && i == 0 {
-				containerId,err = getIDFromContainerLabel(container,tenantId,containerReference)
-			} else {
-				containerId,err = getContainerId(container,tenantId,containerReference)
+			for i, containerReference := range containerReferences {
+				var err error
+				var containerId string
+				if affinityLabelFirst && i == 0 {
+					containerId, err = getIDFromContainerLabel(container, tenantId, containerReference)
+				} else {
+					containerId, err = getContainerId(container, tenantId, containerReference)
+				}
+				if err == nil {
+					m[containerReference] = containerId
+					break
+				}
 			}
-			if err == nil {
-				m[containerReference] = containerId
-				break
-			}			
-		  }
-		}		
+		}
 	}
 	for _, containerReference := range containerReferences {
 		if m[containerReference] == "" {
 			err := fmt.Errorf(NOTAUTHORIZED_ERROR, containerReference)
-			return nil,err
+			return nil, err
 		}
 	}
-	return m,nil
+	return m, nil
 }
 
-func getIDFromContainerLabel(container *cluster.Container, tenantId string, affinityLabelValue string ) (string, error) {
+func getIDFromContainerLabel(container *cluster.Container, tenantId string, affinityLabelValue string) (string, error) {
 	if container.Labels[headers.TenancyLabel] != tenantId {
-		return "",errors.New("not authorized to access this container.")
+		return "", errors.New("not authorized to access this container.")
 	}
 	// affinityLabelValue is in the form label==value
-	kv := strings.Split(affinityLabelValue,"==")
-	for k,v := range container.Config.Labels {
+	kv := strings.Split(affinityLabelValue, "==")
+	for k, v := range container.Config.Labels {
 		if k == kv[0] && v == kv[1] {
-			return container.Info.ID,nil
-		} 
+			return container.Info.ID, nil
+		}
 	}
-	return "",errors.New("label==value does not match this containter.")	
+	return "", errors.New("label==value does not match this containter.")
 }
 
-func getContainerId (container *cluster.Container, tenantId string, containerReference string) (string, error) {
+func getContainerId(container *cluster.Container, tenantId string, containerReference string) (string, error) {
 	if container.Labels[headers.TenancyLabel] != tenantId {
-		return "",errors.New("not authorized to access this container.")
+		return "", errors.New("not authorized to access this container.")
 	}
 	// check for long id
 	if container.Info.ID == containerReference {
-		return container.Info.ID,nil
-    } else if containerReference == container.Labels[headers.OriginalNameLabel] {
-		return container.Info.ID,nil
+		return container.Info.ID, nil
+	} else if containerReference == container.Labels[headers.OriginalNameLabel] {
+		return container.Info.ID, nil
 	} else {
 		// check for name
-	    for _, name := range container.Names {
-		   if containerReference == name  {
-			  return container.Info.ID,nil	   
+		for _, name := range container.Names {
+			if containerReference == name {
+				return container.Info.ID, nil
 			}
 		}
 	}
 	// check for short id
 	if strings.HasPrefix(container.Info.ID, containerReference) {
-		return container.Info.ID,nil
-    }
-	return "",errors.New("containerReference does not match this containter.")
+		return container.Info.ID, nil
+	}
+	return "", errors.New("containerReference does not match this containter.")
 }
-
