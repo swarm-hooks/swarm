33bbf3a4cd5a202864e8a1cb6f54dff803882ddc
andreyod <andreyo@il.ibm.com>
2016-07-11 03:49:03 +0300

    Format and rebase

--- pkg/multiTenancyPlugins/authorization/networks.go
+++ pkg/multiTenancyPlugins/authorization/networks.go
@@ -4,26 +4,26 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"io/ioutil"
-	"net/http"
 	log "github.com/Sirupsen/logrus"
 	apitypes "github.com/docker/engine-api/types"
 	"github.com/docker/swarm/cluster"
 	"github.com/docker/swarm/pkg/multiTenancyPlugins/headers"
 	"github.com/docker/swarm/pkg/multiTenancyPlugins/utils"
 	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
 func ConnectDisconnect(cluster cluster.Cluster, r *http.Request) error {
 	if !utils.IsResourceOwner(cluster, r.Header.Get(headers.AuthZTenantIdHeaderName), mux.Vars(r)["networkid"], "network") {
-			return errors.New("Not authorized or no such network!")
+		return errors.New("Not authorized or no such network!")
 	}
 	defer r.Body.Close()
 	if reqBody, _ := ioutil.ReadAll(r.Body); len(reqBody) > 0 {
 		var request apitypes.NetworkConnect
 		if err := json.NewDecoder(bytes.NewReader(reqBody)).Decode(&request); err != nil {
-         	return err
+			return err
 		}
 		if !utils.IsResourceOwner(cluster, r.Header.Get(headers.AuthZTenantIdHeaderName), request.Container, "container") {
 			return errors.New("Not Authorized or no such container!")
@@ -37,7 +37,6 @@ func ConnectDisconnect(cluster cluster.Cluster, r *http.Request) error {
 	return nil
 }
 
-
 func NetworkAuthorization(cluster cluster.Cluster, r *http.Request, network string) error {
 	// remove this when networks will be created by Swarm only
 	if os.Getenv("SWARM_NETWORK_AUTHORIZATION") == "false" {
@@ -45,11 +44,11 @@ func NetworkAuthorization(cluster cluster.Cluster, r *http.Request, network stri
 		return nil
 	}
 	// allow Docker default networks.
-	if network == "default" || network == "bridge" || network == "host" || network == "none"{
+	if network == "default" || network == "bridge" || network == "host" || network == "none" {
 		return nil
 	}
 	if !utils.IsResourceOwner(cluster, r.Header.Get(headers.AuthZTenantIdHeaderName), network, "network") {
-			return errors.New("Not authorized or no such network!")
+		return errors.New("Not authorized or no such network!")
 	}
 	return nil
 }
