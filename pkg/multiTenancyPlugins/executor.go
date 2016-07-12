ca0ff567af8cab584264e4aceab9df86b286d7db
Ezra Silvera <ezra@il.ibm.com>
2016-07-12 21:49:25 +0300

    fix

--- pkg/multiTenancyPlugins/executor.go
+++ pkg/multiTenancyPlugins/executor.go
@@ -27,10 +27,7 @@ func (*Executor) Handle(cluster cluster.Cluster, swarmHandler http.Handler) http
 		return swarmHandler
 	}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if os.Getenv("SWARM_MULTI_TENANT") == "false" {
-			swarmHandler.ServeHTTP(w, r)
-			return
-		}
+		log.Debug(r)
 		err := startHandler(utils.ParseCommand(r), cluster, w, r, swarmHandler)
 		if err != nil {
 			log.Error(err)
