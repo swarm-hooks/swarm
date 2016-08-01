1bef1f627a6b1ef2b2e252fbabb285ad504a8579
Kenneth Nagin <nagin@il.ibm.com>
2016-08-01 11:19:34 +0300

    testCoverage play

---
+++ rrdns_test.go
@@ -0,0 +1,17 @@
+// +build testrunmain
+
+package main
+
+import (
+         "testing"
+//         "time"
+//         "fmt"
+)
+
+func TestRunMain(t *testing.T) {  
+//    main()
+//    for {
+//        fmt.Println("sleep")
+//        time.Sleep(30000 * time.Millisecond)
+//    }
+}
