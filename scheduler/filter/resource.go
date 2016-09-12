f29508c18253aa99128e2f6c944bccf227ec54a7
andreyod <andreyo@il.ibm.com>
2016-09-12 11:46:04 +0300

    Get resources from env variables.

--- scheduler/filter/resource.go
+++ scheduler/filter/resource.go
@@ -2,8 +2,11 @@ package filter
 
 import (
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/docker/swarm/cluster"
 	"github.com/docker/swarm/scheduler/node"
+	"os"
+	"strconv"
 )
 
 // ResourceFilter selects only nodes based on other containers on the node.
@@ -17,13 +20,35 @@ func (f *ResourceFilter) Name() string {
 
 // Filter is exported
 func (f *ResourceFilter) Filter(config *cluster.ContainerConfig, nodes []*node.Node, _ bool) ([]*node.Node, error) {
+	availableMemory, err := strconv.ParseInt(os.Getenv("SWARM_MEMORY_LIMIT_MB"), 10, 64)
+	if err != nil {
+		log.Warning("Swarm memory limit was not set.")
+		availableMemory = -1
+	} else {
+		availableMemory = availableMemory * 1024 * 1024
+	}
+	availableCpu, err := strconv.ParseInt(os.Getenv("SWARM_CPU_LIMIT"), 10, 64)
+	if err != nil {
+		log.Warning("Swarm CPU limit was not set.")
+		availableCpu = -1
+	}
 	containerMemory := config.HostConfig.Memory
 	containerCpu := config.HostConfig.CPUShares
 	candidates := []*node.Node{}
+	var memoryLimit int64
+	var cpuLimit int64
 	for _, node := range nodes {
-		availableMemory := node.TotalMemory * 50 / 100
-		availableCpu := node.TotalCpus * 50 / 100
-		if node.UsedMemory+containerMemory <= availableMemory && node.UsedCpus+containerCpu <= availableCpu {
+		if availableMemory < 0 || availableMemory > node.TotalMemory {
+			memoryLimit = node.TotalMemory
+		} else {
+			memoryLimit = availableMemory
+		}
+		if availableCpu < 0 || availableCpu > node.TotalCpus {
+			cpuLimit = node.TotalCpus
+		} else {
+			cpuLimit = availableCpu
+		}
+		if node.UsedMemory+containerMemory <= memoryLimit && node.UsedCpus+containerCpu <= cpuLimit {
 			candidates = append(candidates, node)
 		}
 	}
